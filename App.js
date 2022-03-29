3edd3cbfa5ea2a14b756099cd81503f44195a09e
Fahad Mehmood <66324033+fahad0786@users.noreply.github.com>
2022-03-29 23:20:41 +0500

    login.png code

--- App.js
+++ App.js
@@ -1,40 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button ,Image } from 'react-native';
+import { StyleSheet, Text, View , Image , TextInput } from 'react-native';
 
 export default function App() {
   return (
-<>
-    
-    <View style={{ flex: 2.5, backgroundColor: "#f595fe" , borderRadius: 80 , marginLeft:10 ,marginRight:10,marginTop:10}}>
-    <Image source={require("./Saly-1intro-image.png")}></Image>
-    </View>
-    
-    <View style={[styles.container,{backgroundColor:"white"}]}>
-    <Text style={{fontWeight:"bold",fontSize:35,marginTop:60}}> Discover Your </Text>
-    <Text style={{fontWeight:"bold",fontSize:35}}>Own Dream House</Text>
-    <View style={[{alignItems:"center",marginLeft:20,marginTop:20}]}>
-           <Text>Appication login page which is being devlop on react </Text>
-           <Text>native
-      lab work lab task Mad tasks</Text>
-      </View>
-    </View> 
-    <View style={[{flexDirection:"row",marginLeft:130 , marginTop: 100  , paddingHorizontal:20,}]}>
-    
-    <Button title='Sign in'  style={{ backgroundColor:"#f595fe"}}/>
-    <Button title='Register' color="black" ></Button>
-    
-    </View>
-    
-
-  
-    
     <View style={styles.container}>
-    <StatusBar style="auto"/>
+      <Text style={{fontWeight:"bold" , fontSize:35 , marginTop:10}}> Welcome back
+    </Text>
+      <Text style={{paddingTop:10}}> Welcome to the login page of the website work is done on the front login page of the website</Text>
+      <Text style={{paddingBottom:40}}>The work is done on the front side of the page as called as log in page of the website</Text>
+      <TextInput style={{ marginTop:20,width:"20%", borderWidth:3 , backgroundColor:"pink" , fontcolor:"white" , alignContent:"center",textAlign:"center", paddingHorizontal:20,placeholderTextColor:"blue",paddingVertical:20 , borderRadius:2}} placeholder="Username,Email & phonenumber" ></TextInput>
+      <TextInput style={{paddingTop:30 ,width:"20%",placeholderTextColor:"blue", borderWidth:3 , backgroundColor:"pink" , fontcolor:"white" , margin:20 }} placeholder="Password" ></TextInput>
+      <Text style={{marginLeft:300 , textAlign:"justify", alignContent:"center"}}>Forget Password?</Text>
+      <TextInput style={{paddingTop:30 ,width:"20%", borderWidth:3 , backgroundColor:"pink" , color:"white" , margin:20 , aligncontent:"center" , broderradius:20}} placeholder="Sign in" ></TextInput>
+      <Text style={{margin:20, fontWeight:"bold"}}>Or Sign up With</Text>
+      <View style={{width:50,height:50,flex:1, backgroundColor:"white" , flexDirection:"row" ,  marginRight:60}}>
+      <Image source={require("./pic/google.png")} style={{marginRight:20,broderradius:20}}></Image> 
+      <Image source={require("./pic/Facbook.png")}style={{marginRight:20,broderradius:20}}></Image>
+      <Image source={require("./pic/Group.png")}style={{}}></Image>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
-    </>
-    
-  );
   
+  );
 }
 
 const styles = StyleSheet.create({
@@ -44,6 +31,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-})
-
-
+});
